5ec8c6ae208739fa3a239ad6092eaa5dd3435a35
baayso <baayso@outlook.com>
2017-05-03 18:15:15 +0800

    使用avalon遍历创建菜单。

--- html/static/js/menu.js
+++ html/static/js/menu.js
@@ -3,5 +3,19 @@ avalon.config({debug: false});
 var vm = avalon.define({
     $id: 'menu',
     ccc: "测试内容",
-    content: ''
+    data: []
+});
+
+
+// jQuery.support.cors = true;
+$.ajax({
+    url: "static/js/menu.json",
+    // type: 'POST',
+    // data: requestParam,
+    async: false,
+    success: function (result) {
+        vm.data = result;
+    },
+    error: function (e) {
+    }
 });
